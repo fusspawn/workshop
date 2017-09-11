648f608016602ce64f0f1d1889797971aa8506da
ekul6547 <ekul.lamb@live.co.uk>
2017-09-11 10:27:14 +0100

    Matrix to Bat
    Made it to the Bat File

--- run.bat
+++ run.bat
@@ -1,1 +1,22 @@
-echo "hello world";
+@if (@CodeSection == @Batch) @then
+@echo off
+
+rem set SendKeys=CScript //nologo //E:JScript "%~F0"
+rem %SendKeys% "{F11}"
+
+title Matrix
+color 0a
+mode 1000
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+rem PING 1.1.1.1 -n 1 -w 0.2 >NUL
+goto Matrix
+
+goto :EOF
+@end
+
+// JScript section
+var WshShell = WScript.CreateObject("WScript.Shell");
+WshShell.SendKeys(WScript.Arguments(0));
